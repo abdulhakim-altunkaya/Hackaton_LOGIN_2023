6b28bdc5dbfb0984f415b58339142d64d3833b69
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-11 22:12:04 +0300

    netlify fix

--- reserve/LowForm.js
+++ reserve/LowForm.js
@@ -18,6 +18,20 @@ function LowForm() {
     e.preventDefault();
     setDisableButton7(true);
 
+{/*
+
+    const formInput = ` ${contractCode} `;
+    const formInput2 = `
+    Imagine you are smart contract auditor. Take a look at the contract that I will be sharing below.
+    Then try to see loopholes inside the contract. What are the weak points that hackers can exploit? Also,
+    if you detect some vulnerabilities of the contract, mention them and provide solutions and suggestions.
+    Tell us which lines of code should we check, what are the vulnerabilities. However,
+    if you do not see any vulnerability, you can praise. For example, you can say "it is good that you put 
+    require statement here....". Write 8 to 12 sentences in total. Half of them should be about the vulnerabilities.
+    And the other half should be praising the contract code. Do not use repetitive sentences.
+    Here is contract: ${contractCode}
+  `; 
+*/}
     const response = await openai.createCompletion({
         prompt: "how are you",
         model: "text-davinci-003",
